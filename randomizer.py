f6fffc6b90ae2c040d547f712fed60068b89deb4
Paulo Leitão <paulojbleitao@gmail.com>
2024-08-14 00:08:37 -0300

    Add same level randomizer option

--- randomizer.py
+++ randomizer.py
@@ -1,8 +1,13 @@
-import sys
+import argparse
 import yaml # pyyaml dependency
 import random
 
-rathena_path = sys.argv[1] # pass rAthena path through the command line
+parser = argparse.ArgumentParser(prog='Ragnarok Online Randomizer')
+parser.add_argument('-p', '--path')
+parser.add_argument('--same-level', action='store_true')
+args = parser.parse_args()
+
+rathena_path = args.path # pass rAthena path through the command line
 mob_dp_path = f'{rathena_path}/db/pre-re/mob_db.yml'
 prt_fild_path = f'{rathena_path}/npc/pre-re/mobs/fields/prontera.txt'
 mob_db = open(mob_dp_path)
@@ -13,8 +18,15 @@ mob_list = yaml.load(mob_db, Loader=yaml.CLoader)['Body']
 new_file = []
 for line in prt_fild:
     if line.startswith('//') or line == '\n': continue
-    random_mob = random.choice(mob_list)
     new_line = line.split('\t')
+
+    if args.same_level:
+        mob_level = next(filter(lambda mob: mob['Name'] == new_line[2], mob_list))['Level']
+        mobs_with_same_level = list(filter(lambda mob: mob['Level'] == mob_level, mob_list))
+        random_mob = random.choice(mobs_with_same_level)
+    else:
+        random_mob = random.choice(mob_list)
+
     new_line[2] = random_mob['Name']
     id_part = new_line[3].split(',')
     id_part[0] = str(random_mob['Id'])
