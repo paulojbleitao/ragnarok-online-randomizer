c2641284c5efe5cd8cf8f372d15fe7093a1c01b7
Paulo Leitão <paulojbleitao@gmail.com>
2024-08-16 15:41:32 -0300

    Add shuffleMVPs option

--- mobs.py
+++ mobs.py
@@ -2,8 +2,13 @@ import random
 from util import navigate_directories
 from pathlib import Path
 
+def is_mvp(mob_entry):
+    return 'MvpExp' in mob_entry and mob_entry['MvpExp'] > 0
+
 def randomize_mob_file(filepath, mob_list, filtered_mob_list, config):
     level_range = config['levelRange']
+    # maybe find a better name for this? shuffleMVPs means that MVPs will only be randomized between MVPs
+    shuffle_mvps = config['shuffleMVPs']
 
     file = open(filepath)
     new_file = []
@@ -14,13 +19,25 @@ def randomize_mob_file(filepath, mob_list, filtered_mob_list, config):
         # make sure to keep script lines
         if not 'monster' in line or len(new_line) != 4: new_file.append(line); continue
 
+        suitable_mobs = filtered_mob_list
+
+        mob_id = int(new_line[3].split(',')[0])
+        mob_entry = next(filter(lambda mob: mob['Id'] == mob_id, mob_list))
         if level_range >= 0:
-            mob_id = int(new_line[3].split(',')[0])
-            mob_level = next(filter(lambda mob: mob['Id'] == mob_id, mob_list))['Level']
-            mobs_in_level_range = list(filter(lambda mob: mob_level - level_range <= mob['Level'] <= mob_level + level_range, filtered_mob_list))
-            random_mob = random.choice(mobs_in_level_range)
-        else:
-            random_mob = random.choice(filtered_mob_list)
+            mob_level = mob_entry['Level']
+            suitable_mobs = list(filter(lambda mob: mob_level - level_range <= mob['Level'] <= mob_level + level_range, suitable_mobs))
+        if shuffle_mvps and is_mvp(mob_entry):
+            # cursed: for now, if an MVP is not available in the specified level range,
+            # choose between any MVPs that haven't been picked yet
+            temp_suitable_mobs = list(filter(is_mvp, suitable_mobs))
+            if len(temp_suitable_mobs) == 0: temp_suitable_mobs = list(filter(is_mvp, filtered_mob_list))
+            suitable_mobs = temp_suitable_mobs
+        elif shuffle_mvps and not is_mvp(mob_entry):
+            suitable_mobs = list(filter(lambda mob: not is_mvp(mob), suitable_mobs))
+
+        random_mob = random.choice(suitable_mobs)
+        if shuffle_mvps and is_mvp(mob_entry):
+            filtered_mob_list.remove(random_mob) # should guarantee every mvp is available
 
         new_line[2] = random_mob['Name']
         id_part = new_line[3].split(',')
@@ -34,9 +51,9 @@ def randomize_mob_file(filepath, mob_list, filtered_mob_list, config):
         print(line, end='', file=file)
     file.close()
 
-def randomize_mobs(rathena_path, mob_db, level_range):
+def randomize_mobs(rathena_path, mob_db, config):
     mob_list = mob_db['Body']
     filtered_mob_list = list(filter(lambda mob: 'BaseExp' in mob and 'Drops' in mob, mob_list))
     mob_files = navigate_directories(Path(f'{rathena_path}/npc/pre-re/mobs/'), [])
     for file in mob_files:
-        randomize_mob_file(file, mob_list, filtered_mob_list, level_range)
+        randomize_mob_file(file, mob_list, filtered_mob_list, config)
